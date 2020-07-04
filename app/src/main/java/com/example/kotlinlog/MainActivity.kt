a7c6cfc0c29d1117bb0e304fcc44b16eaa85fb0d
KentoTakimoto <kento.zedd2@gmail.com>
2020-07-04 18:48:44 +0900

    エラー

--- app/src/main/java/com/example/kotlinlog/MainActivity.kt
+++ app/src/main/java/com/example/kotlinlog/MainActivity.kt
@@ -1,8 +1,8 @@
 package com.example.kotlinlog
 
-import androidx.appcompat.app.AppCompatActivity
+import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log //追加
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
 
@@ -10,6 +10,13 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        Log.d("kotlintest","ログへの出力テスト")//追加
+        // 整数型の変数をnumという名前で作成して、10を代入する
+        var num = 10
+        Log.d("kotlintest", Integer.toString(num))
+
+        // numに50を代入する
+        // 変数や定数に .toString() と記述する方法もある
+        num = 50
+        Log.d("kotlintest", num.toString())
     }
 }
